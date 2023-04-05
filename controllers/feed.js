593b7f7cb6c41ddd66e04b83936993df01154009
Natasha <natashajanicetambunan@gmail.com>
2023-04-05 17:01:09 +0700

    Add validation with express-validator

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,45 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }],
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/dumbo.jpg',
+        creator: {
+          name: 'Creator',
+        },
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: 'Validation failed, entered data is incorrect',
+        error: errors.array(),
+      });
+  }
   const title = req.body.title;
   const content = req.body.content;
 
   res.status(201).json({
     message: 'Post created successfully',
-    post: { id: new Date().toISOString(), title, content },
+    post: {
+      _id: new Date().toISOString(),
+      title,
+      content,
+      creator: {
+        name: 'Creator',
+      },
+      createdAt: new Date(),
+    },
   });
 };

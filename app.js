72a887ac9d6cffc29f7285f7720b9a4105c97ab4
Natasha <natashajanicetambunan@gmail.com>
2023-04-06 14:18:07 +0700

    Handle storing uploaded file

--- app.js
+++ app.js
@@ -2,13 +2,36 @@ const path = require('path');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuid } = require('uuid');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
+const fileStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'images');
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuid());
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 app.use(express.json());
-app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -23,9 +46,9 @@ app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
-  
+
   res.status(status).json({ message });
-})
+});
 
 mongoose
   .connect('mongodb://0.0.0.0:27017/messages?retryWrites=true&w=majority')

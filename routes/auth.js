533fdf37ee8a15268d3612b3bab74b3e6ec11410
Natasha <natashajanicetambunan@gmail.com>
2023-04-10 11:22:06 +0700

    Add request body validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,37 @@
 const express = require('express');
 
+const User = require('../models/user');
 const authController = require('../controllers/auth');
+
+const { body } = require('express-validator');
 
 const router = express.Router();
 
-router.put('/singup', authController.signup);
+router.put(
+  '/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter valid email')
+      .normalizeEmail()
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject(
+              'Email exist already, please pick up a different one.'
+            );
+          }
+        });
+      }),
+    body('password')
+      .trim()
+      .isLength({ min: 5}),
+    body('name')
+      .trim()
+      .not()
+      .isEmpty()
+  ],
+  authController.signup
+);
 
 module.exports = router;

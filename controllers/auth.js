709d9cf453a3c1663d72a99aca70243fea00c36e
Natasha <natashajanicetambunan@gmail.com>
2023-04-11 09:16:37 +0700

    Add get and update user status

--- controllers/auth.js
+++ controllers/auth.js
@@ -81,3 +81,60 @@ exports.login = (req, res, next) => {
       next(err);
     });
 };
+
+exports.getUserStatus = (req, res, next) => {
+  User.findById(req.userId)
+    .then((user) => {
+      if (!user) {
+        const error = new Error('User not found');
+        error.statusCode = 401;
+        throw error;
+      }
+
+      res
+        .status(200)
+        .json({ message: 'Status fetched!', status: user.status });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+}
+
+exports.updateUserStatus = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const error = new Error();
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+
+  const status = req.body.status;
+
+  User.findById(req.userId)
+    .then((user) => {
+      if (!user) {
+        const error = new Error('Invalid user');
+        error.statusCode = 401;
+        throw error;
+      }
+
+      user.status = status;
+      return user.save();
+    })
+    .then((result) => {
+      res
+        .status(200)
+        .json({ message: 'Status updated!', status: result.status });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};

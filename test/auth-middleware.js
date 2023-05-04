ac765d10ef9bce9fe61792c9976e17068fecdd3a
Natasha <natashajanicetambunan@gmail.com>
2023-05-04 14:52:40 +0700

    Add auth middleware and use stub to mock jwt verify

---
+++ test/auth-middleware.js
@@ -0,0 +1,52 @@
+const expect = require('chai').expect;
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
+
+const authMiddleware = require('../middleware/is-auth');
+
+// To group test
+describe('Auth midleware', function () {
+  it('should throw an error if no authorization header found', function () {
+    const req = {
+      get: function (headerName) {
+        return null;
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      'Not Authenticated'
+    );
+  });
+
+  it('should throw an error if authorization header is only one string', function () {
+    const req = {
+      get: function (headerName) {
+        return 'abc';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should throw an errorif token cannot be verified', function () {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer asdf';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should yield a userId after decoding the token', function () {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer asdf';
+      },
+    };
+    sinon.stub(jwt, 'verify');
+    jwt.verify.returns({ userId: 'abc'})
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property('userId');
+    expect(req).to.have.property('userId', 'abc');
+    expect(jwt.verify.called).to.be.true;
+    jwt.verify.restore();
+  });
+});
